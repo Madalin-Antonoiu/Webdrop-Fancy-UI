069ce7e85c0a79356414c7332fbafa40329a1c33
unknown <antonoiumadalin@gmail.com>
2019-09-05 14:16:31 +0300

    Some progress

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 //To Do - increase z-index on hover
+
+//Mini-Drawer
 function columnOne() {
-  document.getElementById('column1').style.width = "5%";
-  document.getElementById('column1').style.minWidth = "5em";
-  document.getElementById('column2').style.width = "82%";
-  document.getElementById('column3').style.width = "13%";
+  col1 = document.getElementById('column1');
+  col2 = document.getElementById('column2');
+  logotext = document.getElementById('logotext');
+
+  nav1 = document.getElementById('left_part');
+  nav2 = document.getElementById('rest_of_it');
+
+  col1.classList.toggle('column1_mini');
+  col2.classList.toggle('column2_expand');
+
+  nav1.classList.toggle('column1_contract');
+  nav2.classList.toggle('column2_extend');
+  logotext.classList.toggle('displayNone');
 }
